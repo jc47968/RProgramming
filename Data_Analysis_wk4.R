3a8b078dc5af2e92106f0ccf3fcce36a3ddffb98
jc47968 <jasonchoi78@yahoo.com>
2021-12-05 14:49:35 -0800

    Add files via upload

--- Data_Analysis_wk4.R
+++ Data_Analysis_wk4.R
@@ -21,7 +21,8 @@ Mode <- function(x) {
   uq[which.max(tabulate(match(x,uq)))]
 }
 
-##Turnstile Data##
+###Turnstile Data###
+##Data Cleaning##
 
 #Transform Data Type for Date and Time Variables
 TurnData$DATE <- as.Date(TurnData$DATE, "%m/%d/%y")
@@ -29,21 +30,23 @@ TurnData$TIME[1] <- "12:00:00 AM"
 TurnData$TIME <- parse_date_time(TurnData$TIME, "%H:%M:%S, %p")
 TurnData$TIME <- format(as.POSIXct(TurnData$TIME), "%H:%M:%S")
 
-##Data Cleaning##
 str(TurnData) #Review the data structure 
 #change data structure from character to factor for variables: C/A, UNIT, 
 #SCP, STATION,LINENAME,DIVISION, DESC into factors
 TurnNames <- c(2:7,10)
 setDT(TurnData)[,(TurnNames):= lapply(.SD, factor), .SDcols=TurnNames]
+
 #Unique Check
 TurnUnique <-unique(TurnData[,c(1:12)]) #combine all variable and get unique observations
 nrow(TurnUnique)#number of unique rows is 841269
 nrow(TurnData) #number of unique rows is 841269
+
 #Review for String Inconsistencies
-sapply(TurnData[,c(2:7,10)], unique) 
+sapply(TurnData[,c(2:7,10)], unique)#get all unique levels
+sapply(TurnData, function(x) length(unique(x)))#get the count of levels
 
 
-##Data Analysis##
+#Data Analysis for Outliers
 TurnStats <- as.data.frame(summary(TurnData)) #get summary of dataset
 colnames(TurnStats) <-c('N/A', 'Variable', 'Metrics') #rename columns
 TurnStats <- TurnStats[,!names(TurnStats) %in% 'N/A'] #remove "N/A" column
@@ -52,7 +55,11 @@ Mode(TurnData$ENTRIES)#0
 Mode(TurnData$EXITS) #0
 Mode(TurnData$DATE)#"2020-11-07"
 
+#Check For Missing Data
+sum(is.na(TurnData)) #get total missing data in data set
 
+#percentage of missing data for each variable
+apply(TurnData, 2, function(col)sum(is.na(col))/length(col))
 
 
 
